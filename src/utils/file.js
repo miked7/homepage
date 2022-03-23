848c2e5dc37207c4123dc67ac8a813c15175e705
miked7 <miked7@gmail.com>
2022-03-22 17:52:41 -0700

    Fix wrong app name in public Gaia storage hack.

--- src/utils/file.js
+++ src/utils/file.js
@@ -50,9 +50,10 @@ export const readPublicStorageFile = (userX, filename) => {
                    const jsonBlockstack3 = JSON.parse(jsonBlockstack2)
                    jsonBlockstack4 = jsonBlockstack3.localhost
                 }else{
-                  jsonBlockstack2 = jsonBlockstack1.replace("https://xck.app","xckapp");
+                  // AWS hosting URL is something like https://master.d2l0tpetdicwn0.amplifyapp.com.  Replace that with app name.
+                  jsonBlockstack2 = jsonBlockstack1.replace("/.*amplifyapp\.com/","Mixmi");
                   const jsonBlockstack3 = JSON.parse(jsonBlockstack2)
-                  jsonBlockstack4 = jsonBlockstack3.xckapp
+                  jsonBlockstack4 = jsonBlockstack3.Mixmi
                 }
                 const {storage} = jsonBlockstack4
                 const getFile = storage + filename;
