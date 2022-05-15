79a5cd7ff60be3bfbde39d3b4240c532a7b147b8
miked7 <miked7@gmail.com>
2022-05-15 16:44:43 -0700

    Change app domain from test env to prod env.

--- src/utils/environment.js
+++ src/utils/environment.js
@@ -1,8 +1,8 @@
 // IMPORTANT!!!
 // DOMAIN should be set to the host name of the app.
-// For now, while we use mixmi-dev.net for development we substitute mixmi.app so the user names will work.
+// Currently the two domains below are the same.  The could be different if for some text environment for example.
 export const USER_DOMAIN = "mixmi.app";
-export const APP_DOMAIN = "mixmi-dev.net";
+export const APP_DOMAIN = "mixmi.app";
 
 export const DEBUG_USER_NAME = "michael-thompson";
 
